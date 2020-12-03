e6a5f6d5ddeafcf4c901f8bda2053766566a6ece
Kukka Aiko Farza Lawendatu <maulana.ghozi@qbit.co.id>
2020-12-03 16:58:15 +0700

    add function googleAuth and googleAuthRedirect

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('google')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {}
+
+  @Get('redirect')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(@Req() req) {
+    return this.appService.googleLogin(req);
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
